c10b7ff1908817080ce71a6969fab6554983a4bc
Anil_Meran <anilmeran80@gmail.com>
2022-03-27 01:35:47 +0530

    adding multiply.cpp

---
+++ multiply.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2 ,mul;
+    cout<<"Enter the value of num1 ->";
+    cin>>num1;
+    cout<<"Enter the value of num2 ->";
+    cin>>num2;
+    mul=num1*num2;
+    cout<<"Sum is  "<<mul;
+    cout<<"Program is over here"<<endl;
+
+}
