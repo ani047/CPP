bc7931a6ddcc6ebe41843ab1222d3ab4aaf238f1
Anil_Meran <anilmeran80@gmail.com>
2022-03-27 01:18:05 +0530

    adding Diff.cpp

---
+++ diff.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2 ,diff;
+    cout<<"Enter the value of num1 ->";
+    cin>>num1;
+    cout<<"Enter the value of num2 ->";
+    cin>>num2;
+    diff=num1-num2;
+    cout<<"Difference is  "<<diff;
+    cout<<"Program is over here"<<endl;
+
+}
