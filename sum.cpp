2859e8cddcddcfdaabd6b467cbbeecfae0a70930
Anil_Meran <anilmeran80@gmail.com>
2022-03-27 01:11:33 +0530

    This is SUM.CPP

---
+++ sum.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int num1 , num2 ,sum;
+    cout<<"Enter the value of num1 ->";
+    cin>>num1;
+    cout<<"Enter the value of num2 ->";
+    cin>>num2;
+    sum=num1+num2;
+    cout<<"Sum is"<<sum;
+
+}
